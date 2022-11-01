d2be0f77c3eef4489e6f2d2d8d50b57673009f04
“mlnkvao” <belonogov220@gmail.com>
2022-11-01 19:12:19 +0800

    newCommit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("ki");
+        for (int i = 0; i < 100;i++){
+            System.out.println("bib");
+        }
     }
 }
